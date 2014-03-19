d813902f35a8634a21eecbe514b1a5e54f07716b
Caio Carrara <caiocarrara@gmail.com>
2014-03-19 12:39:32 -0300

    Changes to be compatible with Gnome 3.10

--- pixel-saver@deadalnix.me/app_menu.js
+++ pixel-saver@deadalnix.me/app_menu.js
@@ -190,8 +190,8 @@ function enable() {
 	
 	if(Main.panel.statusArea && Main.panel.statusArea["appMenu"]) {
 		appMenu = Main.panel.statusArea["appMenu"];
-	} else if(Main.panel._appMenu) {
-		appMenu = Main.panel._appMenu;
+	} else if(Main.panel.statusArea["appMenu"]) {
+		appMenu = Main.panel.statusArea["appMenu"];
 	} else {
 		appMenu = Main.panel.statusArea.appMenu;
 	}
