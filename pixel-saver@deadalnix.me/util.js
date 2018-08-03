ea29c6db84412ace8d5f25a99abdfff5770efe6b
tallero <purfett@gmail.com>
2018-08-03 10:02:43 +0200

    Shell 3.30 support
    Add wrapper to add 3.30 shell version support while maintaining support for previous versions. Cherry picked from https://github.com/micheleg/dash-to-dock/commit/8398d41c6c122e654daec8c1bf5615c918f99f15

--- pixel-saver@deadalnix.me/util.js
+++ pixel-saver@deadalnix.me/util.js
@@ -3,6 +3,20 @@ const Meta = imports.gi.Meta;
 
 const MAXIMIZED = Meta.MaximizeFlags.BOTH;
 
+// This is wrapper to maintain compatibility with GNOME-Shell 3.30+ as well as
+// previous versions.
+var DisplayWrapper = {
+    getScreen: function() {
+        return global.screen || global.display;
+    },
+     getWorkspaceManager: function() {
+        return global.screen || global.workspace_manager;
+    },
+     getMonitorManager: function() {
+        return global.screen || Meta.MonitorManager.get();
+    }
+};
+
 function getWindow() {
 	// get all window in stacking order.
 	let windows = global.display.sort_windows_by_stacking(
