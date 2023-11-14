3e22e7c6364186fbea84f8c61520f68b7b747d95
Lia Va <mistickusya.2012@gmail.com>
2023-11-14 01:37:03 +0100

    initial

---
+++ pixel-saver-g45@deadalnix.me/util.js
@@ -0,0 +1,62 @@
+const Mainloop = imports.mainloop;
+const Meta = imports.gi.Meta;
+
+const MAXIMIZED = Meta.MaximizeFlags.BOTH;
+
+// This is wrapper to maintain compatibility with GNOME-Shell 3.30+ as well as
+// previous versions.
+var DisplayWrapper = {
+    getScreen: function() {
+        return global.screen || global.display;
+    },
+     getWorkspaceManager: function() {
+        return global.screen || global.workspace_manager;
+    },
+     getMonitorManager: function() {
+        return global.screen || Meta.MonitorManager.get();
+    },
+     getWindowManager: function() {
+	return global.screen || global.window_manager;
+    },
+     getDisplay: function() {
+	return global.screen || global.display;
+    }
+};
+
+function getWindow() {
+	// get all window in stacking order.
+	let windows = global.display.sort_windows_by_stacking(
+		DisplayWrapper.getWorkspaceManager().get_active_workspace().list_windows().filter(function (w) {
+			return w.get_window_type() !== Meta.WindowType.DESKTOP;
+		})
+	);
+	
+	let i = windows.length;
+	while (i--) {
+		let window = windows[i];
+		if (window.get_maximized() === MAXIMIZED && !window.minimized) {
+			return window;
+		}
+	}
+	
+	return null;
+}
+
+function onSizeChange(callback) {
+	let callbackIDs = [];
+	let wm = global.window_manager;
+	
+	// Obvious size change callback.
+	callbackIDs.push(wm.connect('size-change', callback));
+	
+	// Needed for window drag to top panel (this doesn't trigger maximize).
+	callbackIDs.push(wm.connect('hide-tile-preview', callback));
+	
+	// NB: 'destroy' needs a delay for .list_windows() report correctly
+	callbackIDs.push(wm.connect('destroy', function () {
+		Mainloop.idle_add(callback);
+	}));
+	
+	return callbackIDs;
+}
+
